f19e74b4cb96cd13e86918376e4e385fa073b312
David Blackstone <62959991+blackstoneDavidJ@users.noreply.github.com>
2022-05-02 23:44:36 -0400

    Delete addAudio.py

--- addAudio.py
+++
@@ -1,5 +0,0 @@
-import moviepy.editor as mpe
-
-video = mpe.VideoFileClip(r"C:\Users\daveb\Desktop\py\Heathers_Rotoscoped.mp4")
-final_clip = video.set_audio(mpe.AudioFileClip(r"C:\Users\daveb\Desktop\py\audio.mp4"))
-final_clip.write_videofile("heathers_Complete_Rotoscoped.mp4")
